ceb495f417fa1604b9ace31785c0b9d8eb4cfa63
AntimatterReactor <63902100+AntimatterReactor@users.noreply.github.com>
2020-12-23 16:34:00 +0700

    Last optimisation of using cpp

--- src/Compiler.hpp
+++ src/Compiler.hpp
@@ -2,67 +2,66 @@
 #include <vector>
 #include <iostream>
 #include "Token.hpp"
-
 std::string compileToC(std::vector<Token> lex)
 {
     std::string presult;
     for (auto s : lex)
     {
-        if (s.object == '>')
+        switch (s.object)
         {
+        case '>':
             presult += "ptr += " + std::to_string(s.ocount);
             presult += ";\n";
-        }
-        else if (s.object == '<')
-        {
+            break;
+
+        case '<':
             presult += "ptr -= " + std::to_string(s.ocount);
             presult += ";\n";
-        }
-        else if (s.object == '+')
-        {
+            break;
+
+        case '+':
             presult += "ptv[ptr]+=" + std::to_string(s.ocount);
             presult += ";\n";
-        }
-        else if (s.object == '-')
-        {
+            break;
+
+        case '-':
             presult += "ptv[ptr] -= " + std::to_string(s.ocount);
             presult += ";\n";
-        }
-        else if (s.object == '.')
-        {
+            break;
+
+        case '.':
             for (size_t i = 0; i < s.ocount; i++)
             {
                 presult += "putchar(ptv[ptr]);\n";
             }
-        }
-        else if (s.object == ',')
-        {
+            break;
+
+        case ',':
             for (size_t i = 0; i < s.ocount; i++)
             {
                 presult += "ptv[ptr] = getchar();\n";
             }
-        }
-        else if (s.object == '[')
-        {
+            break;
+
+        case '[':
             for (size_t i = 0; i < s.ocount; i++)
             {
                 presult += "while(ptv[ptr]){\n";
             }
-        }
-        else if (s.object == ']')
-        {
+            break;
+
+        case ']':
             for (size_t i = 0; i < s.ocount; i++)
             {
                 presult += "}\n";
             }
+            break;
         }
     }
+
     std::string result;
-
-    result += "int main(){\n\n";
-    result += "char ptv[30000] = {0};\nint ptr = 0;\n";
-    result += presult;
-    result += "\n}";
-
+    sprintf(result.data(),
+            "int main(){\n\nchar ptv[30000] = {0};\nint ptr = 0;\n%s\n}",
+            presult);
     return result;
 }
--- src/sf.cpp
+++ src/sf.cpp
@@ -6,19 +6,12 @@
 #include "Compiler.hpp"
 #include "Token.hpp"
 
-#define COMDEF "\ngcc a.c\n"
-#ifdef __WIN32__
-#define RUNDEF "./a.exe\n"
-#else
-#define RUNDEF "./a.out\n"
-#endif
-
 int main(int argc, char *argv[])
 {
     std::vector<Token> lex;
     if (argc < 2)
     {
-        printf("Usage: Suff.exe [file_path] [output_file_name].c");
+        printf("Usage: Suff.exe [file-path] [output-file-name].c");
         return 0;
     }
     else if (argc = 2)
@@ -27,8 +20,6 @@ int main(int argc, char *argv[])
         std::ofstream fileout("a.c");
         fileout << compileToC(lex);
         fileout.close();
-        system(COMDEF);
-        system(RUNDEF);
     }
     else if (argc = 3)
     {
@@ -36,11 +27,6 @@ int main(int argc, char *argv[])
         std::ofstream fileout(argv[2]);
         fileout << compileToC(lex);
         fileout.close();
-
-        char pla[10];
-        sprintf(pla, "gcc %s -o %s", argv[1], argv[2]);
-
-        system(pla);
     }
 
     return 0;
